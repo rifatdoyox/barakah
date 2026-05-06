e8f748df6f2980f4390a9f3393496a98cd389143
Rifat Doyox <rifatdoyosoft@gmail.com>
2026-05-07 01:39:30 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'icon.png', dest: '.' },
-        { src: 'logo.png', dest: '.' },
-      ],
-    }),
-  ],
+  plugins: [react()],
   base: '/barakah/',
 });
